3d65f32e2164edff3fdf8bdfd392b35f63f56a76
DAkintola94 <degameros@gmail.com>
2025-07-09 12:34:44 +0200

    Post content from react worked, remember to double check URL's in the future

--- LaundryDormReact/laundrydorm_vite/src/ApplicationComponent/Pages/SuccessPage.tsx
+++ LaundryDormReact/laundrydorm_vite/src/ApplicationComponent/Pages/SuccessPage.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useLocation } from 'react-router-dom'
 import { useState } from 'react'
 import { MdError } from 'react-icons/md'
+import { FaCheckCircle } from 'react-icons/fa'
 
 export const SuccessPage = () => {
 const location = useLocation(); //To get value passed from the usenavigate redirection in SettVask.tsx
@@ -15,7 +16,10 @@ const [successId] = useState<number | null>(location.state.ID);
         </div>
 
         ) : 
-        <div> Vask informasjon er lagret. Vask ID er {successId} "</div>
+        <div className="flex items-center justify-center font-bold gap-2"> 
+        <FaCheckCircle className="text-1xl"/>
+        <span> Vask informasjon er lagret. Opprettet vask ID er {successId} </span>
+        </div>
     }
     </>
   )
